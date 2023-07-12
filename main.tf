78b25175d4950a2ad15c12193b6004ee95916c41
KUMAR Sumit OBS/GDO <cmfr2012@orange.com>
2023-07-12 10:48:13 +0530

    my terraform cloud project related demo code

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+provider "aws" {
+  region = "us-west-2"
+  profile = "production"
+}
+
+
+variable "ami_id_map" {
+  type = map(string)
+  default = {
+    "us-west-1" = "ami-0c55b159cbfafe1f0"  # Example AMI ID for us-west-1 region
+    "us-west-2" = "ami-0df24e148fdb9f1d8"  # Example AMI ID for us-west-2 region
+  }
+}
+
+
+resource "aws_instance" "example" {
+  ami           = lookup(var.ami_id_map, "us-west-2", "ami-1234567890")  # Use AMI ID from map, or default to "ami-1234567890"
+  instance_type = "t2.micro"
+  #key_name      = "mykeypair"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
